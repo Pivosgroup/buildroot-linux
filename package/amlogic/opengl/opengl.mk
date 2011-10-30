f426c350b02f750ccf1d2b6c43ba5ce65c6d464a
davilla <davilla@4pi.com>
2011-10-29 20:46:08 -0400

    [pivos] refactor amlogic/opengl install to staging and target

--- package/amlogic/opengl/opengl.mk
+++ package/amlogic/opengl/opengl.mk
@@ -10,12 +10,18 @@ OPENGL_SITE_METHOD=cp
 OPENGL_INSTALL_STAGING=YES
 
 define OPENGL_INSTALL_STAGING_CMDS
-        cp -rfP $(@D)/lib/*.so* $(STAGING_DIR)/usr/lib
-        cp -rf  $(@D)/include/* $(STAGING_DIR)/usr/include
+	install -m 644 $(@D)/lib/*.so* $(STAGING_DIR)/usr/lib
+	cd $(STAGING_DIR)/usr/lib; ln -sf libEGL.so.1.4 libEGL.so.1
+	cd $(STAGING_DIR)/usr/lib; ln -sf libEGL.so.1 libEGL.so
+	cd $(STAGING_DIR)/usr/lib; ln -sf libGLESv1_CM.so.1.1 libGLESv1_CM.so.1
+	cd $(STAGING_DIR)/usr/lib; ln -sf libGLESv1_CM.so.1 libGLESv1_CM.so
+	cd $(STAGING_DIR)/usr/lib; ln -sf libGLESv2.so.2.0 libGLESv2.so.2
+	cd $(STAGING_DIR)/usr/lib; ln -sf libGLESv2.so.2 libGLESv2.so
+	cp -rf  $(@D)/include/* $(STAGING_DIR)/usr/include
 endef
 
 define OPENGL_INSTALL_TARGET_CMDS
-        cp -rfP $(@D)/lib/*.so* $(TARGET_DIR)/usr/lib
+	install -m 644 $(@D)/lib/*.so* $(STAGING_DIR)/usr/lib
 endef
 
 $(eval $(call GENTARGETS,package/amlogic,opengl))
