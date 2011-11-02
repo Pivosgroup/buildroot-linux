5b1f2d635c346ae9f57107ab67146af5d14f995b
theuni <theuni-nospam-@xbmc.org>
2011-11-02 17:35:59 -0400

    [pivos] xbmc: add init script
    With this, xbmc runs on startup. chmod -x the script to disable.
    
    Also, xbmc symlink created for convenience. To start/stop xbmc use:
    /etc/init.d/xbmc start|stop|restart

---
+++ package/amlogic/xbmc/xbmc.mk
@@ -0,0 +1,11 @@
+XBMC_VERSION = dummy
+XBMC_INSTALL_TARGET = YES
+XBMC_SOURCE=xbmc-$(XBMC_VERSION).tar.gz
+XBMC_SITE=./package/amlogic/xbmc
+XBMC_SITE_METHOD=cp
+
+define XBMC_INSTALL_TARGET_CMDS
+  cp -rf package/amlogic/xbmc/etc $(TARGET_DIR)
+endef
+
+$(eval $(call GENTARGETS,package/amlogic,xbmc))
