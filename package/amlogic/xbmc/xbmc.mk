92f2725dc5f62eb45e38837cd962f8dad9c8ea47
theuni <theuni-nospam-@xbmc.org>
2012-02-03 21:19:47 -0500

    [aml] add a real xbmc package.
    Deps still need cleaning/testing, but I've verified that XBMC can build
    with only the packages provided.

--- package/amlogic/xbmc/xbmc.mk
+++ package/amlogic/xbmc/xbmc.mk
@@ -1,12 +1,50 @@
-XBMC_VERSION = dummy
+################################################################################
+#
+## xbmc
+#
+#################################################################################
+
+XBMC_VERSION = 17f022f41c3fe426b80f144b08a50d2290723e07
+XBMC_SITE_METHOD = git
+XBMC_SITE = ssh://git@github.com/Pivosgroup/xbmc.git
+XBMC_INSTALL_STAGING = YES
 XBMC_INSTALL_TARGET = YES
-XBMC_SOURCE=xbmc-$(XBMC_VERSION).tar.gz
-XBMC_SITE=./package/amlogic/xbmc
-XBMC_SITE_METHOD=cp
+
 XBMC_DEPENDENCIES = host-lzo host-sdl_image
 
-define XBMC_INSTALL_TARGET_CMDS
+XBMC_CONF_OPT+=--enable-gles --disable-dvdcss \
+  --disable-x11 --disable-libbluray --disable-sdl --enable-directfb \
+  --disable-rsxs --disable-projectm --disable-non-free --enable-neon --disable-optical-drive \
+  --enable-external-ffmpeg --disable-joystick --with-platform=amlogic-m1
+
+#--with-platform=amlogic
+
+XBMC_DEPENDENCIES += libogg flac libmad libmpeg2 libogg \
+  libsamplerate libtheora libvorbis wavpack bzip2 dbus libcdio \
+  python lzo zlib libgcrypt openssl mysql_client sqlite fontconfig \
+  freetype jasper jpeg libmodplug libpng libungif tiff libcurl \
+  libmicrohttpd libssh2 boost fribidi ncurses pcre libnfs \
+  readline expat libxml2 yajl samba libass
+
+XBMC_CONF_ENV += PYTHON_VERSION="$(PYTHON_VERSION_MAJOR)"
+XBMC_CONF_ENV += PYTHON_LDFLAGS="-L$(STAGING_DIR)/usr/lib/ -lpython$(PYTHON_VERSION_MAJOR) -lpthread -ldl -lutil -lm"
+XBMC_CONF_ENV += PYTHON_CPPFLAGS="-I$(STAGING_DIR)/usr/include/python$(PYTHON_VERSION_MAJOR)"
+XBMC_CONF_ENV += PYTHON_SITE_PKG="$(STAGING_DIR)/usr/lib/python$(PYTHON_VERSION_MAJOR)/site-packages"
+XBMC_CONF_ENV += PYTHON_NOVERSIONCHECK="no-check"
+XBMC_CONF_ENV += USE_TEXTUREPACKER_NATIVE_ROOT="$(HOST_DIR)/usr"
+
+# For braindead apps like mysql that require running a binary/script
+XBMC_CONF_ENV += PATH=$(STAGING_DIR)/usr/bin:$(TARGET_PATH)
+
+define XBMC_BOOTSTRAP
+  cd $(XBMC_DIR) && ./bootstrap
+endef
+
+define XBMC_INSTALL_ETC
   cp -rf package/amlogic/xbmc/etc $(TARGET_DIR)
 endef
 
-$(eval $(call GENTARGETS,package/amlogic,xbmc))
+XBMC_PRE_CONFIGURE_HOOKS += XBMC_BOOTSTRAP
+XBMC_POST_INSTALL_TARGET_HOOKS += XBMC_INSTALL_ETC
+
+$(eval $(call AUTOTARGETS,package/amlogic,xbmc))
