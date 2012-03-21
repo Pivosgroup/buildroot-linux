427240dd4a0dca391e11b07e985e6d177ab9c589
theuni <theuni-nospam-@xbmc.org>
2012-03-21 14:56:02 -0400

    [aml] add xios skin

---
+++ package/thirdparty/xios-skin/xios.mk
@@ -0,0 +1,24 @@
+XIOS_VERSION = 9b7f9d160a0d6a0190ed6abc2f4cefec13eced94
+XIOS_SITE_METHOD = git
+XIOS_SITE = git@github.com:Pivosgroup/skin.pivos.git
+XIOS_INSTALL_STAGING = YES
+XIOS_INSTALL_TARGET = YES
+XIOS_DEPENDENCIES = xbmc
+TEXTURE_PACKER=$(XBMC_DIR)/tools/TexturePacker/TexturePacker
+
+define XIOS_BUILD_CMDS
+	$(TEXTURE_PACKER) -use_none -i $(@D)/media -o $(@D)/media/Textures.xbt
+endef
+
+define XIOS_INSTALL_STAGING_CMDS
+        mkdir -p $(STAGING_DIR)/usr/share/xbmc/addons/skin.xios
+        cp -rf $(@D)/* $(STAGING_DIR)/usr/share/xbmc/addons/skin.xios/
+endef
+
+define XIOS_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/share/xbmc/addons/skin.xios/media
+	cd $(@D); cp -rf `ls -I media -1` $(TARGET_DIR)/usr/share/xbmc/addons/skin.xios
+	cp $(@D)/media/Textures.xbt $(TARGET_DIR)/usr/share/xbmc/addons/skin.xios/media
+endef
+
+$(eval $(call GENTARGETS,package/thirdparty,xios))
