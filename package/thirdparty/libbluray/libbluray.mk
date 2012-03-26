cf26c5e923100b484f891da33877278526b8b7a8
davilla <davilla@4pi.com>
2012-03-26 11:45:07 -0400

    [aml] add libbluray

---
+++ package/thirdparty/libbluray/libbluray.mk
@@ -0,0 +1,13 @@
+#############################################################
+#
+# libbluray
+#
+#############################################################
+LIBBLURAY_VERSION = 0.2.2
+LIBBLURAY_SITE = ftp://ftp.videolan.org/pub/videolan/libbluray/$(LIBBLURAY_VERSION)
+LIBBLURAY_SOURCE = libbluray-$(LIBBLURAY_VERSION).tar.bz2
+LIBBLURAY_INSTALL_STAGING = YES
+LIBBLURAY_INSTALL_TARGET = YES
+LIBBLURAY_AUTORECONF = YES
+
+$(eval $(call AUTOTARGETS,package/thirdpardy,libbluray))
