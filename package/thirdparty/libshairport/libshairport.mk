d59b94c03b00f9ab1d7ab4e57332391e9506f702
davilla <davilla@4pi.com>
2012-03-26 11:46:23 -0400

    [aml] add libshairport

---
+++ package/thirdparty/libshairport/libshairport.mk
@@ -0,0 +1,14 @@
+#############################################################
+#
+# libshairport
+#
+#############################################################
+LIBSHAIRPORT_VERSION = 1.2.0.20310_lib
+LIBSHAIRPORT_SITE = http://mirrors.xbmc.org/build-deps/darwin-libs
+LIBSHAIRPORT_SOURCE = libshairport-$(LIBSHAIRPORT_VERSION).tar.gz
+LIBSHAIRPORT_INSTALL_STAGING = YES
+LIBSHAIRPORT_INSTALL_TARGET = YES
+LIBSHAIRPORT_AUTORECONF = YES
+LIBSSH2_DEPENDENCIES += openssl
+
+$(eval $(call AUTOTARGETS,package/thirdpardy,libshairport))
