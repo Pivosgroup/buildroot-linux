4ba113b989b6697a0c4081149f4ae7f10c92615a
theuni <theuni-nospam-@xbmc.org>
2012-04-06 04:36:25 -0400

    [aml] working udev. doesn't work with bluetooth though (the irony)
    Still not complete. Inittab and some services need to be cleaned, a few hotplug
    events can be added, bluetooth coldplug needs to be fixed, and need to get rid
    of mdev left-overs.
    
    IMPORTANT: (at least) the following must be enabled in the kernel if using
    udev with devtmpfs:
      CONFIG_DEVTMPFS=y
      CONFIG_DEVTMPFS_MOUNT=y
    
    It would be wise to 'make clean' for buildroot as well, otherwise lots of
    /dev/* cruft will be hanging around.
    
    udev-trigger_happy.patch: though we have the kernel defines necessary, our
      toolchain headers are older than 2.6.34. Quick fix is just to define it.
    
    udev-accept4.patch: fixes "udev unable to receive ctrl connection" spew
    
    Do not go higher than 173 any time soon as 174 shifts lots of things around.
    
    acl's disabled to avoid the dep that we don't need.
    
    id's fix backported from buildroot master.

--- package/udev/udev.mk
+++ package/udev/udev.mk
@@ -5,7 +5,7 @@
 #############################################################
 UDEV_VERSION = 173
 UDEV_SOURCE = udev-$(UDEV_VERSION).tar.bz2
-UDEV_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/kernel/hotplug/
+UDEV_SITE = http://linuxfromscratch.pl/pub/LFS/lfs-packages/7.0
 UDEV_INSTALL_STAGING = YES
 
 UDEV_CONF_OPT =			\
@@ -17,11 +17,11 @@ UDEV_CONF_OPT =			\
 UDEV_DEPENDENCIES = host-gperf host-pkg-config
 
 ifeq ($(BR2_PACKAGE_UDEV_ALL_EXTRAS),y)
-UDEV_DEPENDENCIES += libusb libusb-compat acl usbutils hwdata libglib2
+UDEV_DEPENDENCIES += libusb libusb-compat usbutils hwdata libglib2
 UDEV_CONF_OPT +=							\
-	--with-pci-ids-path=$(TARGET_DIR)/usr/share/hwdata/pci.ids	\
-	--with-usb-ids-path=$(TARGET_DIR)/usr/share/hwdata/usb.ids	\
-	--enable-udev_acl
+	--with-pci-ids-path=/usr/share/hwdata/pci.ids	\
+	--with-usb-ids-path=/usr/share/hwdata/usb.ids	\
+
 else
 UDEV_CONF_OPT +=		\
 	--disable-hwdb		\
