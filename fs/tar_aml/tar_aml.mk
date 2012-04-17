ec7a0fa99574c759261bfbffb43a90220b24cd1a
theuni <theuni-nospam-@xbmc.org>
2012-04-17 00:06:12 -0400

    [aml] hook up recovery image creator
    Note that host Java and zip are required, but won't be added here.
    
    Untested, but output looks correct.

--- fs/tar_aml/tar_aml.mk
+++ fs/tar_aml/tar_aml.mk
@@ -16,10 +16,16 @@ TAR_AML_SQUASHFS_ARGS += -comp gzip
 endif
 endif
 
+TAR_AML_CLEANUP = true
+ifneq ($(BR2_TARGET_ROOTFS_RECOVERY_AML),y)
+TAR_AML_CLEANUP = rm -f $(TARGET_DIR)/usr.sqsh
+endif
+
 define ROOTFS_TAR_AML_CMD
  $(HOST_DIR)/usr/bin/mksquashfs $(TARGET_DIR)/usr $(TARGET_DIR)/usr.sqsh $(TAR_AML_SQUASHFS_ARGS) -noappend -all-root && \
- tar --anchored --exclude=\"./usr/*\" -cf $(BINARIES_DIR)/rootfs.tar -C $(TARGET_DIR) . && \
- rm $(TARGET_DIR)/usr.sqsh
+ tar --anchored --exclude=\"./usr/*\" -cf $(BINARIES_DIR)/rootfs.tar -C $(TARGET_DIR) . && $(TAR_AML_CLEANUP)
 endef
+
+
 
 $(eval $(call ROOTFS_TARGET,tar_aml))
