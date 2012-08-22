cf495a3529e3cc1c9f449ccaf560808459c8a3b8
Cory Fields <theuni-nospam-@xbmc.org>
2012-08-22 13:42:07 -0400

    [aml] fix build. No need for custom install, buildroot cleans up after us

--- package/thirdparty/fribidi/fribidi.mk
+++ package/thirdparty/fribidi/fribidi.mk
@@ -9,9 +9,9 @@ FRIBIDI_SITE = http://fribidi.org/download
 FRIBIDI_INSTALL_STAGING = YES
 FRIBIDI_INSTALL_TARGET = YES
 
-define FRIBIDI_INSTALL_TARGET_CMDS
-       mkdir -p $(TARGET_DIR)/usr/lib
-       cp -dpf $(@D)/.libs/libfribidi.so* $(TARGET_DIR)/usr/lib/
-endef
+#define FRIBIDI_INSTALL_TARGET_CMDS
+#       mkdir -p $(TARGET_DIR)/usr/lib
+#       cp -dpf $(@D)/.libs/libfribidi.so* $(TARGET_DIR)/usr/lib/
+#endef
 
 $(eval $(call AUTOTARGETS,package/thirdparty,fribidi))
