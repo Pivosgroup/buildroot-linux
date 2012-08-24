a1f7245d415585b85d5c727385cb79b0cd9530af
davilla <davilla@4pi.com>
2012-08-23 20:14:03 -0400

    [aml] change jasper to new dl location

--- package/thirdparty/jasper/jasper.mk
+++ package/thirdparty/jasper/jasper.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 JASPER_VERSION = 1.900.1
-JASPER_SITE = http://sources.openelec.tv/devel
+JASPER_SITE = http://www.ece.uvic.ca/~frodo/jasper/software
 JASPER_SOURCE = jasper-$(JASPER_VERSION).tar.bz2
 JASPER_INSTALL_STAGING = YES
 JASPER_INSTALL_TARGET = YES
