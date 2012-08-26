2253d2e7d0955e5f20aaa8a785cc511251745275
davilla <davilla@4pi.com>
2012-08-26 14:08:56 -0400

    [aml] changed amffmpeg to use external librtmp

--- package/amlogic/amffmpeg/amffmpeg.mk
+++ package/amlogic/amffmpeg/amffmpeg.mk
@@ -8,7 +8,7 @@ AMFFMPEG_SOURCE=amffmpeg-$(AMFFMPEG_VERSION).tar.gz
 AMFFMPEG_SITE=./package/amlogic/libplayer/src/amffmpeg
 AMFFMPEG_SITE_METHOD=cp
 AMFFMPEG_INSTALL_STAGING=YES
-AMFFMPEG_DEPENDENCIES = alsa-lib
+AMFFMPEG_DEPENDENCIES = alsa-lib librtmp
 
 AMFFMPEG_CONF_OPT= --disable-static --enable-shared \
                 --disable-ffmpeg --disable-ffplay --disable-ffserver --disable-doc \
@@ -16,7 +16,8 @@ AMFFMPEG_CONF_OPT= --disable-static --enable-shared \
                 --disable-amd3dnow --disable-amd3dnowext --disable-mmx --disable-mmx2 --disable-sse --disable-ssse3 \
                 --disable-armv5te --disable-armv6t2 --disable-iwmmxt --disable-mmi --disable-vis --disable-yasm \
                 --enable-cross-compile --arch=arm --cpu=cortex-a9 --enable-neon --target-os=linux \
-                --enable-pthreads --enable-runtime-cpudetect --enable-pic --enable-avfilter --enable-postproc --enable-gpl
+                --enable-pthreads --enable-runtime-cpudetect --enable-pic --enable-avfilter --enable-postproc --enable-gpl \
+                --enable-librtmp --pkg-config=pkg-config
 
 define AMFFMPEG_CONFIGURE_CMDS
 	(cd $(AMFFMPEG_SRCDIR) && rm -rf config.cache && \
