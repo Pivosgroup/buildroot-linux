a8b9c35531646277c7661144b2d2cacf551fb03b
davilla <davilla@4pi.com>
2012-09-06 14:01:20 -0400

    Revert "[aml] bump libnfs to the version other OSs are using"
    This reverts commit 8de26d9c66f6a40081694f980b247406a297c1cc.

--- package/thirdparty/libnfs/libnfs.mk
+++ package/thirdparty/libnfs/libnfs.mk
@@ -3,7 +3,7 @@
 # libnfs
 #
 #############################################################
-LIBNFS_VERSION = libnfs-1.3.0
+LIBNFS_VERSION = 0804e67d7a512585cebd3c453e5d05986b8ad218
 LIBNFS_SITE = git://github.com/sahlberg/libnfs.git
 LIBNFS_INSTALL_STAGING = YES
 LIBNFS_INSTALL_TARGET = YES
