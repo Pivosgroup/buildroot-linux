f1e5d17d690f9226e9c42578fa5a28ae63c3ba34
Cory Fields <theuni-nospam-@xbmc.org>
2012-09-06 18:52:57 -0400

    [aml] add tvheadend package

---
+++ package/thirdparty/tvheadend/tvheadend.mk
@@ -0,0 +1,9 @@
+TVHEADEND_VERSION = 3.0
+TVHEADEND_SITE_METHOD = git
+TVHEADEND_SITE = git://github.com/tvheadend/tvheadend.git
+TVHEADEND_INSTALL_STAGING = YES
+TVHEADEND_INSTALL_TARGET = YES
+TVHEADEND_DEPENDENCIES = v4lutils openssl \
+                         $(if $(BR2_PACKAGE_TVHEADEND_AVAHI),avahi)
+
+$(eval $(call AUTOTARGETS,package/thirdparty,tvheadend))
