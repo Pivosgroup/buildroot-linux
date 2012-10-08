59d2ac0dad9d004e933306023cb7bc825f8e6a3f
davilla <davilla@4pi.com>
2012-10-07 20:38:24 -0400

    [aml] libnfs can fail to build using -jN, N > 1, force it to 1

--- package/thirdparty/libnfs/libnfs.mk
+++ package/thirdparty/libnfs/libnfs.mk
@@ -8,5 +8,6 @@ LIBNFS_SITE = git://github.com/sahlberg/libnfs.git
 LIBNFS_INSTALL_STAGING = YES
 LIBNFS_INSTALL_TARGET = YES
 LIBNFS_AUTORECONF = YES
+LIBNFS_MAKE=$(MAKE1)
 
 $(eval $(call AUTOTARGETS,package/thirdparty,libnfs))
