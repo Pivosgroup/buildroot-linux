6568392687abc5c96579b8e1f86ad5bb15ac769f
J1nx <admin@j1nx.nl>
2012-12-05 12:47:59 +0000

    update ubootaml makefile

--- package/amlogic/ubootaml/ubootaml.mk
+++ package/amlogic/ubootaml/ubootaml.mk
@@ -5,9 +5,9 @@ UBOOTAML_INSTALL_STAGING=YES
 UBOOTAML_DEPENDENCIES=ubootamltoolchain
 
 define UBOOTAML_BUILD_CMDS
-	rm -f $(@D)/tools/ucl/libucl_linux.lib
+	rm -rf $(@D)/tools/ucl/libucl_linux
 	$(MAKE) -C $(@D)/tools/ucl/ucl_cygwin_prj/
-        cp -f $(@D)/tools/ucl/ucl_cygwin_prj/objs/libucl.a $(@D)/tools/ucl/libucl_linux.lib
+        cp -f $(@D)/tools/ucl/ucl_cygwin_prj/ucl_linux/libucl_linux.a $(@D)/tools/ucl/libucl_linux.lib
         $(MAKE) -C $(@D) m3_mbox_config
         PATH="$(TOOLCHAIN_EXTERNAL_DIR)/bin:$(TARGET_PATH)" $(MAKE) -C $(@D)
 endef
