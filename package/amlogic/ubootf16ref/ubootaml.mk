8829746bdc0017fed553c390a7af9f3e9cb46250
J1nx <admin@j1nx.nl>
2013-01-05 13:03:48 +0000

    [f16ref] Move away the uboot to it's own f16ref version, restore the Pivos one for the other boxes (for now)

--- package/amlogic/ubootf16ref/ubootaml.mk
+++ package/amlogic/ubootf16ref/ubootaml.mk
@@ -1,14 +1,15 @@
-UBOOTAML_VERSION:=50dba3182b40ff71a68ddae1ba88a53cdab93935
-UBOOTAML_SITE=git@github.com:Pivosgroup/buildroot-uboot.git
+UBOOTAML_VERSION:=0648a177f3ae87445f02003d52eeff3687ac165c
+UBOOTAML_SITE=git@github.com:j1nx/Amlogic-reff16-uboot.git
 UBOOTAML_SITE_METHOD=git
 UBOOTAML_INSTALL_STAGING=YES
 UBOOTAML_DEPENDENCIES=ubootamltoolchain
 
 define UBOOTAML_BUILD_CMDS
-	rm -f $(@D)/tools/ucl/libucl_linux.lib
+	rm -f $(@D)/tools/ucl/libucl_linux.lib	
+	rm -rf $(@D)/tools/ucl/ucl_cygwin_prj/ucl_linux
 	$(MAKE) -C $(@D)/tools/ucl/ucl_cygwin_prj/
-        cp -f $(@D)/tools/ucl/ucl_cygwin_prj/objs/libucl.a $(@D)/tools/ucl/libucl_linux.lib
-        $(MAKE) -C $(@D) stv_mbx_m3_512_config
+        cp -f $(@D)/tools/ucl/ucl_cygwin_prj/ucl_linux/libucl_linux.a $(@D)/tools/ucl/libucl_linux.lib
+        $(MAKE) -C $(@D) m3_mbox_config
         PATH="$(TOOLCHAIN_EXTERNAL_DIR)/bin:$(TARGET_PATH)" $(MAKE) -C $(@D)
 endef
 
