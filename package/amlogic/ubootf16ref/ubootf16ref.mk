16bb4ac5fdb72b822b9640c3d6364c11bd74bfb5
J1nx <admin@j1nx.nl>
2013-01-07 10:31:16 +0000

    [aml] More changes to support multiple boardnames

--- package/amlogic/ubootf16ref/ubootf16ref.mk
+++ package/amlogic/ubootf16ref/ubootf16ref.mk
@@ -1,14 +1,10 @@
-UBOOTF16REF_VERSION:=0648a177f3ae87445f02003d52eeff3687ac165c
+UBOOTF16REF_VERSION:=03f6b6659603eec35d62cd8e46d3036faff65641
 UBOOTF16REF_SITE=git://github.com/j1nx/Amlogic-reff16-uboot.git
 UBOOTF16REF_SITE_METHOD=git
 UBOOTF16REF_INSTALL_STAGING=YES
 UBOOTF16REF_DEPENDENCIES=ubootamltoolchain
 
 define UBOOTF16REF_BUILD_CMDS
-	rm -f $(@D)/tools/ucl/libucl_linux.lib	
-	rm -rf $(@D)/tools/ucl/ucl_cygwin_prj/ucl_linux
-	$(MAKE) -C $(@D)/tools/ucl/ucl_cygwin_prj/
-        cp -f $(@D)/tools/ucl/ucl_cygwin_prj/ucl_linux/libucl_linux.a $(@D)/tools/ucl/libucl_linux.lib
         $(MAKE) -C $(@D) m3_mbox_config
         PATH="$(TOOLCHAIN_EXTERNAL_DIR)/bin:$(TARGET_PATH)" $(MAKE) -C $(@D)
 endef
