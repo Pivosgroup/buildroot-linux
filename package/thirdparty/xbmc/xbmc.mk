c3745cef347129a171dda007ab28b527acd2d4fd
davilla <davilla@4pi.com>
2013-02-14 22:12:46 -0500

    [aml] bump xbmc version to pickup guisettings overrides v3

--- package/thirdparty/xbmc/xbmc.mk
+++ package/thirdparty/xbmc/xbmc.mk
@@ -4,7 +4,7 @@
 #
 #################################################################################
 
-XBMC_VERSION = f3d3c28db7d1385eb8aa522ddf7fab285e65c493
+XBMC_VERSION = b5d00b6f918d8013baa35981eeabee8215355efe
 XBMC_SITE_METHOD = git
 XBMC_SITE = git://github.com/Pivosgroup/xbmc.git
 XBMC_INSTALL_STAGING = YES
