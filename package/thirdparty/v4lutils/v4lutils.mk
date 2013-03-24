af982d069431a485554719888e67d77b2570f059
JoeKar <fake@fake.mail>
2013-03-24 18:02:58 +0100

    Update of tvheadend to newest stable and fixed v4lutils dependency

--- package/thirdparty/v4lutils/v4lutils.mk
+++ package/thirdparty/v4lutils/v4lutils.mk
@@ -3,6 +3,7 @@ V4LUTILS_SOURCE = v4l-utils-$(V4LUTILS_VERSION).tar.bz2
 V4LUTILS_SITE = http://linuxtv.org/downloads/v4l-utils
 V4LUTILS_INSTALL_STAGING = YES
 V4LUTILS_INSTALL_TARGET = YES
+V4LUTILS_DEPENDENCIES = jpeg
 
 define V4LUTILS_BUILD_CMDS
 	# v4l tries to build a qt app with all the wrong paths if qt is installed. Make sure it doesn't find it.
