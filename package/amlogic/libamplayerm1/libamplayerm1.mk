a8d76b4f0758da9d7a8c355a0ddeb3b0b1ebcf38
Trent Nelson <trent.nelson@pivosgroup.com>
2013-08-16 17:41:46 -0400

    Move libamplayer to binary distribution for M1

--- package/amlogic/libamplayerm1/libamplayerm1.mk
+++ package/amlogic/libamplayerm1/libamplayerm1.mk
@@ -4,10 +4,11 @@
 #
 #############################################################
 LIBAMPLAYERM1_VERSION:=e7ba30082fcf17874d1091a455ddaf9f4070a4fa
-LIBAMPLAYERM1_SITE=git://github.com/Pivosgroup/libamplayer-m1.git
+LIBAMPLAYERM1_SOURCE=libamplayerm1-$(LIBAMPLAYERM1_VERSION).tar.gz
+LIBAMPLAYERM1_SITE=$(TOPDIR)/package/amlogic/libamplayerm1/src
 LIBAMPLAYERM1_INSTALL_STAGING=YES
 LIBAMPLAYERM1_INSTALL_TARGET=YES
-LIBAMPLAYERM1_SITE_METHOD=git
+LIBAMPLAYERM1_SITE_METHOD=cp
 
 ifeq ($(BR2_PACKAGE_LIBAMPLAYERM1),y)
 # actually required for amffmpeg
@@ -21,35 +22,31 @@ define LIBAMPLAYERM1_BUILD_CMDS
  $(call AMFFMPEG_CONFIGURE_CMDS)
  $(call AMFFMPEG_BUILD_CMDS)
  $(call AMFFMPEG_INSTALL_STAGING_CMDS)
-
- mkdir -p $(STAGING_DIR)/usr/include/amlplayer
- $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" HEADERS_DIR="$(STAGING_DIR)/usr/include/amlplayer" \
-  CROSS_PREFIX="$(TARGET_CROSS)" SYSROOT="$(STAGING_DIR)" PREFIX="$(STAGING_DIR)/usr" -C $(@D)/amadec install
- $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" HEADERS_DIR="$(STAGING_DIR)/usr/include/amlplayer" CROSS_PREFIX="$(TARGET_CROSS)" \
-  SYSROOT="$(STAGING_DIR)" PREFIX="$(STAGING_DIR)/usr" SRC=$(@D)/amcodec -C $(@D)/amcodec install
- $(MAKE) CROSS="$(TARGET_CROSS)" CC="$(TARGET_CC)" LD="$(TARGET_LD)" PREFIX="$(STAGING_DIR)/usr" \
-  SRC="$(@D)/amplayer" -C $(@D)/amplayer
 endef
 
 define LIBAMPLAYERM1_INSTALL_STAGING_CMDS
- $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" INSTALL_DIR="$(STAGING_DIR)/usr/lib" \
-  STAGING="$(STAGING_DIR)/usr" PREFIX="$(STAGING_DIR)/usr" -C $(@D)/amplayer install
+	mkdir -p $(STAGING_DIR)/usr/include
+	install -m 644 $(@D)/usr/include/*.h $(STAGING_DIR)/usr/include
+	mkdir -p $(STAGING_DIR)/usr/include/amlplayer
+	install -m 644 $(@D)/usr/include/amlplayer/*.h $(STAGING_DIR)/usr/include/amlplayer
+	mkdir -p $(STAGING_DIR)/usr/include/amlplayer/amports
+	install -m 644 $(@D)/usr/include/amlplayer/amports/*.h $(STAGING_DIR)/usr/include/amlplayer/amports
+	mkdir -p $(STAGING_DIR)/usr/include/amlplayer/ppmgr
+	install -m 644 $(@D)/usr/include/amlplayer/ppmgr/*.h $(STAGING_DIR)/usr/include/amlplayer/ppmgr
+	mkdir -p $(STAGING_DIR)/usr/lib
+	install -m 755 $(@D)/usr/lib/*.so* $(STAGING_DIR)/usr/lib
 
- #temporary, until we sync with mainline xbmc
- cp -rf $(@D)/amcodec/include/* $(STAGING_DIR)/usr/include
+	#temporary, until we sync with mainline xbmc
+	cp -rf $(@D)/usr/include/amlplayer/* $(STAGING_DIR)/usr/include
 endef
 
 define LIBAMPLAYERM1_INSTALL_TARGET_CMDS
- $(call AMFFMPEG_INSTALL_TARGET_CMDS)
+	$(call AMFFMPEG_INSTALL_TARGET_CMDS)
 
- mkdir -p $(TARGET_DIR)/lib/firmware
- cp -rf $(@D)/amadec/firmware/*.bin $(TARGET_DIR)/lib/firmware
- cp -f $(STAGING_DIR)/usr/lib/libamadec.so $(TARGET_DIR)/usr/lib/
-
- cp -f $(STAGING_DIR)/usr/lib/libamcodec.so.* $(TARGET_DIR)/usr/lib/
- cp -f $(STAGING_DIR)/usr/lib/libamplayer.so $(STAGING_DIR)/usr/lib/libamcontroler.so $(TARGET_DIR)/usr/lib/
- $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" INSTALL_DIR="$(TARGET_DIR)/usr/lib" \
-  STAGING="$(TARGET_DIR)/usr" PREFIX="$(STAGING_DIR)/usr" -C $(@D)/amplayer install
+	mkdir -p $(TARGET_DIR)/lib/firmware
+	install -m 644 $(@D)/lib/firmware/*.bin $(TARGET_DIR)/lib/firmware
+	mkdir -p $(TARGET_DIR)/usr/lib
+	install -m 755 $(@D)/usr/lib/*.so* $(TARGET_DIR)/usr/lib
 endef
 
 $(eval $(call GENTARGETS,package/amlogic,libamplayerm1))
---
+++ package/amlogic/libamplayerm1/src/amffmpeg/libavformat/Android.mk
@@ -0,0 +1,30 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+include $(LOCAL_PATH)/../common.mk
+LOCAL_SRC_FILES := $(FFFILES)
+LOCAL_C_INCLUDES :=		\
+	$(LOCAL_PATH)		\
+	$(LOCAL_PATH)/..	\
+	$(LOCAL_PATH)/../../amavutils/include/	\
+	external/zlib
+LOCAL_CFLAGS += $(FFCFLAGS)
+LOCAL_MODULE := $(FFNAME)
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+include $(LOCAL_PATH)/../common.mk
+LOCAL_SRC_FILES := $(FFFILES)
+LOCAL_C_INCLUDES :=		\
+	$(LOCAL_PATH)		\
+	$(LOCAL_PATH)/..	\
+	$(LOCAL_PATH)/../../amavutils/include/	\
+	external/zlib
+LOCAL_CFLAGS += $(FFCFLAGS)
+LOCAL_MODULE := $(FFNAME)
+LOCAL_SHARED_LIBRARIES +=   libutils libmedia libz libbinder libdl libcutils libc libavutil libavcodec libamavutils
+LOCAL_PRELINK_MODULE := false
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)
+
+# Reset CC as it's overwritten by common.mk
+CC := $(HOST_CC)
