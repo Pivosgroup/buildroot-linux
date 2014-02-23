2f22fc658450f37714ec87e60b0c06e2f605b23a
Daniel Burr <dburr@topcon.com>
2014-02-23 18:49:04 +0100

    [aml] Rename xios to xios-skin
    Fix xios-skin package: it seems that newer versions of buildroot require
    that the package name matches the directory name

--- package/thirdparty/xios-skin/xios-skin.mk
+++ package/thirdparty/xios-skin/xios-skin.mk
@@ -1,21 +1,21 @@
-XIOS_VERSION = 78e564bc2d8411a328459b9957741cd3e3493074
-XIOS_SITE_METHOD = git
-XIOS_SITE = git://github.com/Pivosgroup/skin.pivos.git
-XIOS_INSTALL_STAGING = YES
-XIOS_INSTALL_TARGET = YES
-XIOS_DEPENDENCIES = xbmc
+XIOS_SKIN_VERSION = 78e564bc2d8411a328459b9957741cd3e3493074
+XIOS_SKIN_SITE_METHOD = git
+XIOS_SKIN_SITE = git://github.com/Pivosgroup/skin.pivos.git
+XIOS_SKIN_INSTALL_STAGING = YES
+XIOS_SKIN_INSTALL_TARGET = YES
+XIOS_SKIN_DEPENDENCIES = xbmc
 TEXTURE_PACKER=$(XBMC_DIR)/tools/TexturePacker/TexturePacker
 
-define XIOS_BUILD_CMDS
+define XIOS_SKIN_BUILD_CMDS
 	$(TEXTURE_PACKER) -use_none -i $(@D)/media -o $(@D)/media/Textures.xbt
 endef
 
-define XIOS_INSTALL_STAGING_CMDS
+define XIOS_SKIN_INSTALL_STAGING_CMDS
         mkdir -p $(STAGING_DIR)/usr/share/xbmc/addons/skin.xios
         cp -rf $(@D)/* $(STAGING_DIR)/usr/share/xbmc/addons/skin.xios/
 endef
 
-define XIOS_INSTALL_TARGET_CMDS
+define XIOS_SKIN_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/share/xbmc/addons/skin.xios/media
 	cd $(@D); cp -rf `ls -I media -1` $(TARGET_DIR)/usr/share/xbmc/addons/skin.xios
 	cp -f $(@D)/media/Textures.xbt $(TARGET_DIR)/usr/share/xbmc/addons/skin.xios/media
