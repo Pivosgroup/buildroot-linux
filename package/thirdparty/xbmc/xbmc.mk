7e19bc5336e6a7ca3a903b41eca0f6739082e5eb
the-hydra <xphile@gmx.us>
2014-04-22 21:39:11 +1000

    Frodo 12.3 update

--- package/thirdparty/xbmc/xbmc.mk
+++ package/thirdparty/xbmc/xbmc.mk
@@ -4,7 +4,7 @@
 #
 #################################################################################
 
-XBMC_VERSION = 4dd128f49e34e8dc348c7ba108377a8cc02ddf76
+XBMC_VERSION = d18e2626728b6011604d996c91aff4d4c661caa8
 XBMC_SITE_METHOD = git
 XBMC_SITE = git://github.com/the-hydra/xbmc.git
 XBMC_INSTALL_STAGING = YES
